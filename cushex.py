cef15812c7e7e71af5399d83f7f3442e12707b8a
adexter <adexter@accertify.com>
2018-07-05 09:04:47 -0500

    Created a driver script, changed script names and made base64 work in non ideal situations

--- cushex.py
+++ cushex.py
@@ -14,8 +14,3 @@ def hexToBin( value ):
         result.append(binaryHex[i])
     return ''.join(result)
 
-print(hexToBin("49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d"))
-
-#    x = int(hex, 16)
-#    return bin(x);
-
---
+++ driver.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python
+
+import cusbase64
+import cushex
+
+print(cusbase64.binToBase64(cushex.hexToBin("41")))
