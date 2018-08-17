40255dcfc6785614d10be1247ffa6f4fa0ec61de
root <dexmount@hotmail.com>
2018-08-17 07:49:26 -0500

    started analyze method

--- analyze.py
+++ analyze.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python
+import string
 
 def analyze(value):
+    value = value.replace(" ", "")
+    value = value.lower()
+    value.translate(None, string.punctuation)
+    characters = list(set(value))
 
